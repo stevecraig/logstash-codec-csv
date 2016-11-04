db34a53425b423405fec8cabe46f046e649bcdc4
Steve Craig <Stephen.Craig@sky.uk>
2016-11-04 16:38:04 +0000

    initial commit

---
+++ lib/logstash/codecs/csv.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+require "csv"
+require "logstash/codecs/base"
+require "logstash/util/charset"
+
+# The "plain" codec is for plain text with no delimiting between events.
+#
+# This is mainly useful on inputs and outputs that already have a defined
+# framing in their transport protocol (such as zeromq, rabbitmq, redis, etc)
+class LogStash::Codecs::CSV < LogStash::Codecs::Base
+  config_name "csv"
+
+
+  config :csv_options, :validate => :hash, :required => false, :default => Hash.new
+  config :spreadsheet_safe, :validate => :boolean, :default => true
+  config :fields, :validate => :array, :required => true
+
+  public
+  def register
+    @csv_options = Hash[@csv_options.map{|(k, v)|[k.to_sym, v]}]
+  end
+
+  public
+  #def decode(data)
+  #  yield LogStash::Event.new("message" => @converter.convert(data))
+  #end # def decode
+
+  public
+  def encode(event)
+    csv_values = @fields.map {|name| get_value(name, event)}
+    @on_event.call(event,csv_values.to_csv(@csv_options))
+  end # def encode
+
+  private
+  def get_value(name, event)
+    val = event.get(name)
+    val.is_a?(Hash) ? LogStash::Json.dump(val) : escape_csv(val)
+  end
+
+  private
+  def escape_csv(val)
+    (spreadsheet_safe && val.is_a?(String) && val.start_with?("=")) ? "'#{val}" : val
+  end
+end # class LogStash::Codecs::Plain
---
+++ spec/codecs/csv_spec.rb
@@ -0,0 +1,55 @@
+# encoding: utf-8
+require "logstash/devutils/rspec/spec_helper"
+require "logstash/codecs/csv"
+
+
+describe LogStash::Codecs::CSV do
+    describe "['test1','test2','test3']" do
+      subject do
+        LogStash::Codecs::CSV.new({ "fields" => ["test1","test2","test3"]})
+      end
+      it 'should output empty fields if field is missing' do
+        event = LogStash::Event.new({"test1" => "myvalue"})
+
+        subject.on_event do |ev,output|
+          expect(output).to match(/myvalue,,/)
+        end
+
+        subject.encode(event)
+      end
+
+      it 'should output empty fields when no fields are given' do
+        event = LogStash::Event.new({})
+
+        subject.on_event do |ev,output|
+          expect(output).to match(/,,/)
+        end
+
+        subject.encode(event)
+      end
+
+      it 'should output correct field in correct place' do
+        event = LogStash::Event.new({"test3" => "testvalue"})
+
+        subject.on_event do |ev,output|
+          expect(output).to match(/,,testvalue/)
+        end
+
+        subject.encode(event)
+      end
+    end
+    describe "col_sep" do
+       subject do
+        LogStash::Codecs::CSV.new({ "fields" => ["test1","test2","test3"],"csv_options" => {"col_sep" => ";"}})
+      end
+      it 'should output correct sep' do
+        event = LogStash::Event.new({"test3" => "testvalue3","test2" => "testvalue2","test1" => "testvalue1"})
+
+        subject.on_event do |ev,output|
+          expect(output).to match(/testvalue1;testvalue2;testvalue/)
+        end
+
+        subject.encode(event)
+      end
+    end
+end
